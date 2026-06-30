d2d9f23ac9406566f428e96321613707bc0350af
Nisha Verma <148184779+xnisha-verma@users.noreply.github.com>
2026-06-30 23:39:48 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+               return isPalindrome(s, l+1, r) || isPalindrome(s,l,r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    boolean isPalindrome(String s, int l, int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
